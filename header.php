6fde6991f52893718f5284582393e43ce9340745
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-27 15:57:18 +0100

    Added Bootstrap 4 grid classes to header top-bar section

--- header.php
+++ header.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 	<div id="page" class="site">
 		<header>
 			<section class="search">Search</section>
 			<section class="top-bar">
-				<div class="brand">Logo</div>
-				<div class="second-column">
-					<div class="acount">Account</div>
-					<nav class="main-menu">Menu</nav>
+				<div class="container">
+					<div class="row">
+						<div class="brand col-3">Logo</div>
+						<div class="second-column col-9">
+							<div class="acount">Account</div>
+							<nav class="main-menu">Menu</nav>
+						</div>						
+					</div>
 				</div>
 			</section>
 		</header>
