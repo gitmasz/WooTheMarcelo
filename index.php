8ce7849f37e89d74556e56a81de38cd082243565
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-27 15:19:44 +0100

    Added header, footer and index starting content

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<title>WooCommerce Marcelo Theme</title>
-</head>
-<body <?php body_class(); ?>>
-	<p>Hello world! Test paragraph.</p>
-</body>
-</html>
+<?php get_header(); ?>
+		<div class="content-area">
+			<main>
+				<section class="slider">Slider</section>
+				<section class="popular-products">Popular Products</section>
+				<section class="new-arrivals">New Arrivals</section>
+				<section class="deal-of-the-week">Deal of the Week</section>
+				<section class="lab-blog">News</section>
+			</main>
+		</div>
+<?php get_footer(); ?>
