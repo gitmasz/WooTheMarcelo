7dba2d8294c373800545c7d71bdb44227357daff
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-27 15:14:46 +0100

    Theme basis

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>WooCommerce Marcelo Theme</title>
+</head>
+<body <?php body_class(); ?>>
+	<p>Hello world! Test paragraph.</p>
+</body>
+</html>
