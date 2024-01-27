56642b115d23537c1fe3bb7f9c31fae20749b79b
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-27 16:17:50 +0100

    Registered main menu

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@ function woothe_marcelo_scripts(){
   wp_enqueue_style( 'woothe-marcelo', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'woothe_marcelo_scripts' );
+
+function woothe_marcelo_config(){
+	register_nav_menus(
+		array(
+			'woothe_marcelo_main_menu' 	=> __('Main Menu', 'woothe-marcelo')
+		)
+	);
+}
+
+add_action( 'after_setup_theme', 'woothe_marcelo_config', 0 );
