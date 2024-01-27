43b4908c7566b28fecb68049513640af113e9d3d
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-27 15:49:07 +0100

    Added Bootstrap 4 framework

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 
 function woothe_marcelo_scripts(){
+  wp_enqueue_script( 'bootstrap-4', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
+  wp_enqueue_style( 'bootstrap-4', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.3.1', 'all' );
   wp_enqueue_style( 'woothe-marcelo', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'woothe_marcelo_scripts' );
