de9532e0531f04f292845c2a1822f9db2575915d
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-29 17:47:29 +0100

    Added cart icon and link

--- functions.php
+++ functions.php
@@ -42,3 +42,15 @@ add_action( 'after_setup_theme', 'woothe_marcelo_config', 0 );
 if( class_exists( 'WooCommerce' )){
 	require get_template_directory() . '/inc/woocommerce-modifications.php';
 }
+
+add_filter( 'woocommerce_add_to_cart_fragments', 'woothe_marcelo_woocommerce_header_add_to_cart_fragment' );
+
+function woothe_marcelo_woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+	ob_start();
+	?>
+	<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+	<?php
+	$fragments['span.items'] = ob_get_clean();
+	return $fragments;
+}
