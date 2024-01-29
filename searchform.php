c0c461cd041355ff5fae2e8bc2216ebb86680520
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-29 17:36:49 +0100

    Limited search to WooCommerce products

--- searchform.php
+++ searchform.php
@@ -1,4 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input type="search" placeholder="<?php echo __('Search &hellip;', 'woothe-marcelo'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	<button type="submit"><span class="screen-reader-text"><?php echo __('Search;', 'woothe-marcelo'); ?></span></button>
+	<?php if( class_exists( 'WooCommerce' )): ?>
+		<input type="hidden" value="product" name="post_type" id="post_type">
+	<?php endif; ?>
 </form>
